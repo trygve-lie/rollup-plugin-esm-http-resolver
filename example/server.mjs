15f74f2ca54782c0b0ae5286467f1f7599b6a433
Trygve Lie <post@trygve-lie.com>
2019-06-27 13:45:57 +0200

    Restructure assets

--- example/server.mjs
+++ example/server.mjs
@@ -5,7 +5,7 @@ import url from 'url';
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 const start = async () => {
-    const server = new HttpServer({ wwwroot: `${__dirname}/public`});
+    const server = new HttpServer({ wwwroot: `${__dirname}/../utils/modules/simple`});
     const addr = await server.listen(9000);
     console.log('HttpServer is running at:', addr);
 };
